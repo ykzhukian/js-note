0e8cf94bfd0fc92a089070b865d01ceda7b0ad06
Kian Zhu <kianzyk@gmail.com>
2018-07-06 14:24:21 +0800

    grid

--- src/js/components/Grid.js
+++ src/js/components/Grid.js
@@ -5,15 +5,15 @@ export default class Grid extends Component {
   render() {
     return (
       <div className="grid-container">
-          <div className="item"><span>1</span></div>
-          <div className="item"><span>2</span></div>
-          <div className="item"><span>3</span></div>
-          <div className="item"><span>4</span></div>
-          <div className="item"><span>5</span></div>
-          <div className="item"><span>6</span></div>
-          <div className="item"><span>7</span></div>
-          <div className="item"><span>8</span></div>
-          <div className="item"><span>9</span></div>
+          <div className="item item-1"><span>1</span></div>
+          <div className="item item-2"><span>2</span></div>
+          <div className="item item-3"><span>3</span></div>
+          <div className="item item-4"><span>4</span></div>
+          <div className="item item-5"><span>5</span></div>
+          <div className="item item-6"><span>6</span></div>
+          <div className="item item-7"><span>7</span></div>
+          <div className="item item-8"><span>8</span></div>
+          <div className="item item-9"><span>9</span></div>
       </div>
     )
   }
