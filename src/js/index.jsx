4dbcecbc00b6fbf56a2c9f982337ac0fdfc6cf78
Kian Zhu <kianzyk@gmail.com>
2018-10-26 16:35:05 +0800

    index

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { sum } from './utils';
-
 import App from './components/App';
 
 import '../css/application.scss';
-
-console.log(sum);
 
 render(<App />, document.getElementById('app'));
