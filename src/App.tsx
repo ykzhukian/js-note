0d8b8d818665486d25de3d2b1e97663b53b6088c
Kian Zhu <kianzyk@gmail.com>
2019-08-29 16:15:57 +0800

    try dat.gui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import DatGui from '@/components/DatGui';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>render nothing</p>
+        <DatGui />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/DatGui/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as dat from 'dat.gui';
+
+export default class DatGui extends React.Component {
+  gui = new dat.GUI();
+  state = {
+    name: 'Sam',
+  };
+
+  componentDidMount() {
+    this.gui.addFolder('flow field');
+
+    const person = { name: this.state.name };
+    this.gui.add(person, 'name').onFinishChange((name: string) => {
+      this.setState({ name });
+    });
+  }
+  render() {
+    return (
+      <div>{this.state.name}</div>
+    );
+  }
+}
